150813edaafb86f606e6b7dc61596a76e4c9a69e
Kevin C. Krinke <kevin@krinke.ca>
2023-11-30 14:28:05 -0500

    util (httperror): purge SendError runtime.Caller code, be/pk/log already handles this; add *http.Request to SendError call signature; use log.ErrorRDF to log both request and caller info

--- util/httperror.go
+++ util/httperror.go
@@ -2,23 +2,14 @@ package util
 
 import (
 	"net/http"
-	"path/filepath"
-	"runtime"
 
 	"github.com/go-enjin/github-com-craftamap-atlas-gonnect"
 
 	"github.com/go-enjin/be/pkg/log"
 )
 
-func SendError(w http.ResponseWriter, addon *gonnect.Addon, errorCode int, message string) {
+func SendError(w http.ResponseWriter, r *http.Request, addon *gonnect.Addon, errorCode int, message string) {
 	w.WriteHeader(errorCode)
 	_, _ = w.Write([]byte(message))
-	pc, file, no, ok := runtime.Caller(1)
-	details := runtime.FuncForPC(pc)
-
-	if ok {
-		log.ErrorF("%s:%d %s() %s", filepath.Base(file), no, details.Name(), message)
-	} else {
-		log.ErrorF(message)
-	}
+	log.ErrorRDF(r, 1, "%s", message)
 }
